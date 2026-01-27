18e24f4524533ee3c392030301c668859e159157
Natai Freitas Araujo <nataifreitas2025@gmail.com>
2026-01-27 19:21:29 -0300

    andamento

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function entrarPagina(){
     var cpf_usuario, senha_usuario;
 
+    var tipoUsuario = "aluno";
+
     cpf_usuario = document.getElementById('inpUsuario').value;
     senha_usuario = document.getElementById('inpSenha').value;
 
@@ -13,6 +15,19 @@ function entrarPagina(){
     else{
 
         alert("Usuario e senha incorretos! <br>")
+    
+    }
+
+    function selecionarTipo(tipo, botao) {
+        tipoUsuario = tipo;
+      
+        document.querySelectorAll('.btn-tipo').forEach(btn => {
+          btn.classList.remove('ativo');
+        });
+      
+        botao.classList.add('ativo');
+      
+        console.log("Tipo selecionado:", tipoUsuario);
     }
 
 
